fd8f242d2cff3a9f68d6531f02e7d907b38670bc
efe <eefee1987@gmail.com>
2023-10-19 17:59:39 +0200

    finish image feature

--- src/controllers/meetups/createMeetup.js
+++ src/controllers/meetups/createMeetup.js
@@ -19,7 +19,7 @@ const createMeetup = async (req, res, next) => {
       const photo = req.files.photo;
       processPhoto = await processAndSaveImage(photo.data);
     } else {
-      processPhoto = 'avatar-default.png';
+      processPhoto = 'default-meetup-image.jpg';
     }
 
     const meetup_id = await insertMeetup({
